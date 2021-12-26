a688103ec9c486dedc8ae2845b891a82e53944be
moesaid <masedup@gmail.com>
2021-12-25 19:31:19 -0500

    Added -- String methods

--- lib/horus.dart
+++ lib/horus.dart
@@ -1,7 +1,6 @@
 library horus;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+import 'dart:math';
+
+// string work
+part 'src/string.dart';
