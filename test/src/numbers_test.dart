4fde7d39fdcdfdd995b39955dc8aa96997ab4156
moesaid <masedup@gmail.com>
2021-12-26 19:09:17 -0500

    Added — numbers testing

--- test/src/numbers_test.dart
+++ test/src/numbers_test.dart
@@ -1,11 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:horus/horus.dart';
 
 void main() {
   // test for all the methods in HourusNumber
 
-  // abs
-  test('abs', () {
-    expect(42.abs(), 42);
-    expect(0.abs(), 0);
+  // abbreviate
+  test('abbreviate', () async {
+    expect(1000.abbreviate(), '1.0K');
+  });
+
+  // currencyFormat
+  test('currencyFormat', () async {
+    expect(1000.currencyFormat(), '\$1,000.00');
+  });
+
+  // enforcePrecision
+  test('enforcePrecision', () async {
+    expect(0.615.enforcePrecision(2), 0.61);
+  });
+
+  // ordinal
+  test('ordinal', () async {
+    expect(1.ordinal(), '1st');
+    expect(2.ordinal(), '2th');
   });
 }
